eb43fa8a0a1c2ad8da9976b66042f900a7543b39
emolinab <mbacca@pik-potsdam.de>
2021-02-23 18:07:14 +0100

    getnames overall costs output fixed

--- R/calcValidOverallCosts.R
+++ R/calcValidOverallCosts.R
@@ -4,23 +4,23 @@
 #' @return List of magpie object with results on country level, no weight, unit and description.
 #' @author Edna J. Molina Bacca
 #' @examples
-#' \dontrun{ 
+#' \dontrun{
 #' calcOutput("calcValidOverallCosts")
 #' }
- 
-calcValidOverallCosts <- function(datasource="FAO") {
-  
-  #Value of Production for the agriculture, forestry and fisheries sector
-  
-  if(datasource == "FAO"){
-    
-  x<-dimSums(calcOutput("VoP_AFF",aggregate = FALSE),dim=c(3))/(1+0.04)^15 #Original costs in 2020 values. Conversion factor for USD 05.
-  weight=NULL
-  units="mio.05USD"
-  
-  }
-  
-  return(list(x=x,weight=weight,unit=units,description="Value of Production for Agriculture, Forestry and Fisheries"))
-  
-}
 
+calcValidOverallCosts <- function(datasource="FAO") {
+
+  #Value of Production for the agriculture, forestry and fisheries sector
+
+  if(datasource == "FAO"){
+
+  x<-dimSums(calcOutput("VoP_AFF",aggregate = FALSE),dim=c(3))/(1+0.04)^15 #Original costs in 2020 values. Conversion factor for USD 05.
+  getNames(x) <- paste0("Costs|MainSolve"," (million US$05/yr)")
+  weight=NULL
+  units="(million US$05/yr)"
+
+  }
+
+  return(list(x=x,weight=weight,unit=units,description="Value of Production for Agriculture, Forestry and Fisheries"))
+
+}
