cd72b28d55b14e5f9473494b18e7f1598475c966
emolinab <mbacca@pik-potsdam.de>
2021-04-09 15:10:22 +0200

    typo in ValidCostsOverall

--- R/calcValidCostsOverall.R
+++ R/calcValidCostsOverall.R
@@ -1,14 +1,14 @@
-#' @title calcValidCostOverall
+#' @title calcValidCostsOverall
 #' @description Returns historical values of the overall value of production for the Agricultural, Forestry sectors, and fisheries (mio.05USD)
 #' @param datasource datasource for validation. FAO Value of Production dataset.
 #' @return List of magpie object with results on country level, no weight, unit and description.
 #' @author Edna J. Molina Bacca
 #' @examples
 #' \dontrun{
-#' calcOutput("calcValidCostOverall")
+#' calcOutput("calcValidCostsOverall")
 #' }
 
-calcValidCostOverall <- function(datasource="FAO") {
+calcValidCostsOverall <- function(datasource="FAO") {
 
   ####Value of Production for the agriculture, forestry and fisheries sector
 
@@ -16,7 +16,7 @@ calcValidCostOverall <- function(datasource="FAO") {
 
   x<-dimSums(calcOutput("VoP_AFF",aggregate = FALSE),dim=3.1)/(1+0.04)^15 # Original costs in 2020 values. Conversion factor for US$D05.
 
-  #Fraction of revenue
+  #Fraction of reven
 
   rev<-readSource("TFP_USDA")[,,"revenue"]
   rev<-time_interpolate(rev,interpolated_year = c((getYears(rev,as.integer = TRUE)+5)),
