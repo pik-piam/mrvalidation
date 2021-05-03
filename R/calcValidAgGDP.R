68913eac27a654dd109b933873396caae3cc5848
emolinab <mbacca@pik-potsdam.de>
2021-05-03 08:50:05 +0200

    Valid.scripts for Ag.l GDP, and Ag. Forestry and Fisheries GDP added

--- R/calcValidAgGDP.R
+++ R/calcValidAgGDP.R
@@ -1,75 +1,69 @@
 #' @title calcValidAgGDP
-#' @description validation for agricultural, fisheries, forestry value added gdp (Million 05USD)
-#' @param datasource datasource for validation, WDI and FAO
+#' @description Validation for agricultural value added gdp (Million 05USD)
+#' @param datasource datasource for validation (FAO)
 #' @return List of magpie object with results on country level, no weight, unit and description.
-#' @author David Chen, Edna J. Molina Bacca
+#' @author Edna J. Molina Bacca
 #' @importFrom magclass collapseNames
+#' @importFrom magpiesets findset
 #' @examples
 #' \dontrun{
 #' calcOutput("ValidAgGDP")
 #' }
 
-calcValidAgGDP<- function(datasource="WDI") {
-
-  if (datasource=="WDI") {
-    AgG <-  readSource("WDI",subtype="NV.AGR.TOTL.KD")
-    #deflate from constant 2010 to constant 2005 $
-    gdpC <- readSource("WDI",subtype="NY.GDP.MKTP.CD")
-    gdpC <- collapseNames(gdpC)
-
-    gdp2010 <- readSource("WDI",subtype="NY.GDP.MKTP.KD")
-    gdp2010 <- collapseNames(gdp2010)
-
-    infl <- gdpC[,2005,]/gdp2010[,2005,]
-    infl[is.na(infl)] <- 0
-    infl[is.infinite(infl)] <- 0
-
-    AgG05 <- AgG*setYears(infl,NULL)
-
-    out <- collapseNames(AgG05)
-    #set na's to 0
-
-  } else if (datasource == "FAO"){
-
+calcValidAgGDP<- function(datasource="FAO") {
+  
+   if (datasource == "FAO"){
+    
     Prod_kcr<-collapseNames(calcOutput("Production",products="kcr",aggregate= FALSE,attributes="dm"))
     Prod_kli<-collapseNames(calcOutput("Production",products="kli",aggregate= FALSE,attributes="dm"))
-
-    prices_kcr<-calcOutput("PricesProducer",products = "kcr",aggregate=FALSE)[,2005,]
-    prices_kli<-calcOutput("PricesProducer",products = "kli",aggregate=FALSE)[,2005,]
-
+    
+    prices_kcr<-setYears(calcOutput("PricesProducer",products = "kcr",aggregate=FALSE)[,2005,],NULL)
+    prices_kli<-setYears(calcOutput("PricesProducer",products = "kli",aggregate=FALSE)[,2005,],NULL)
+    
     names_kcr<-intersect(getNames(Prod_kcr),getNames(prices_kcr))
     names_kli<-intersect(getNames(Prod_kli),getNames(prices_kli))
-
-    Vprod_all<- dimSums(Prod_kcr[,,names_kcr]*setYears(prices_kcr[,,names_kcr],NULL),dim=3)+
-    dimSums(Prod_kli[,,names_kli]*setYears(prices_kli[,,names_kli],NULL),dim=3)
-
-    # Secondary seed and feed
-        
-    data <- readSource("FAO", "Fodder")#FAO_online showing errors
-    # load sectoral mapping
-    aggregation <- toolGetMapping("FAOFodder_magpieFEED_mapping.csv",type="sectoral")
-    # sectoral aggregation of data  
-    demand_feed<- collapseNames(toolAggregate(data, rel=aggregation, from="ProductionItem", to="MAgPIE_FEED_items", dim=3.1, partrel = TRUE)[,,"production"])
-        
-    prices<-calcOutput("PriceAgriculture",datasource="FAO",aggregate=FALSE)
-    prices_FF<-mbind(prices[,,"historical.FAO.maiz"],prices[,,"historical.FAO.maiz"],prices[,,"historical.FAO.others"])
-    getNames(prices_FF)<-getNames(demand_feed)
-
-    Vdemand<-dimSums(demand_feed[,,]*setYears(prices_FF[,2005,],NULL),dim=3)
-
-    years<-intersect(getYears(Vdemand),getYears(Vprod_all))
+    years<-intersect(getYears(Prod_kli),getYears(Prod_kcr))
+    
+    Vprod_all<- dimSums(Prod_kcr[,years,names_kcr]*prices_kcr[,,names_kcr],dim=3)+
+      dimSums(Prod_kli[,years,names_kli]*prices_kli[,,names_kli],dim=3)
+    
+    ## Secondary seed and feed
+    
+    #Seed
+    seed_kcr<-collapseNames(calcOutput("Seed",products="kcr",attributes="dm",aggregate = FALSE))
+    seed_kli<-collapseNames(calcOutput("Seed",products="kli",attributes="dm",aggregate = FALSE))
+    
+    #feed
+    feed<-collapseNames((calcOutput("FAOmassbalance",aggregate = FALSE)[,,"feed"])[,,"dm"])
+    kcr<-findset("kcr")
+    kli<-findset("kli")
+    
+    feed_kcr<-feed[,,kcr]
+    feed_kli<-feed[,,kli]
+    
+    #Price consumers (World Prices)
+    prices_kcr_con<-setYears(calcOutput("IniFoodPrice",products = "kcr",aggregate=FALSE,convert=FALSE),NULL)
+    prices_kli_con<-setYears(calcOutput("IniFoodPrice",products = "kli",aggregate=FALSE,convert=FALSE),NULL)
+    
+    years<-intersect(getYears(seed_kcr),
+                     intersect(getYears(feed_kcr),getYears(Vprod_all)))
+    
+    Vdemand<-dimSums((seed_kcr[,years,]+feed_kcr[,years,])*prices_kcr_con,dim=3)+
+      dimSums((seed_kli[,years,]+feed_kli[,years,])*prices_kli_con,dim=3)
+    
     out<-Vprod_all[,years,]-Vdemand[,years,]
-
-
-  }
-  else {stop("unknown datasource")}
-
-  getNames(out) <- "Value|Value added agriculture forestry fishery"
+    out[out<0]<-0
+    
+    
+   }else{ 
+    stop("unknown datasource")}
+  
+  getNames(out) <- "Value|Agriculture GDP"
   out <- add_dimension(out, dim=3.1, add="scenario", nm="historical")
   out <- add_dimension(out, dim=3.2, add="model", nm=datasource)
-
+  
   return(list(x=out,
               weight=NULL,
               unit="Million US$05",
-              description="Agriculture Fisheries Forestry Value added GDP"))
+              description="Agriculture Value added GDP"))
 }
