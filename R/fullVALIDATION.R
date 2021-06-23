999f4d77c5aee5413e2a7699d269f96dd9117b72
Felicitas <beier@pik-potsdam.de>
2021-06-23 11:32:28 +0200

    included ValidCostsAEI in fullValidation

--- R/fullVALIDATION.R
+++ R/fullVALIDATION.R
@@ -179,6 +179,7 @@ fullVALIDATION <- function(rev = 0.1) {
   calcOutput(type = "ValidCostsFertilizer", datasource = "Vittis", aggregate = "REG+GLO", file = valfile, append = TRUE, try = TRUE)
   calcOutput(type = "ValidCostsLabor", datasource = "Vittis", aggregate = "REG+GLO", file = valfile, append = TRUE, try = TRUE)
   calcOutput(type = "ValidCostsTC", datasource = "Pardey", aggregate = "REG+GLO", file = valfile, append = TRUE, try = TRUE)
-  # calcOutput(type="ValidCostsAEI", datasource="IMPACT", aggregate="REG+GLO", file=valfile, append=TRUE, try=TRUE)
+  calcOutput(type = "ValidCostsAEI", datasource = "IMPACT", aggregate = "REG+GLO", file = valfile, append = TRUE, try = TRUE)
+  # should we include discounting of costs? If so: where? in each of the costs functions individually?
 
 }
