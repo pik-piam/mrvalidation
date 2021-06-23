601f0960e9aee3c0c2357662b3b13dc284f0c0a6
Felicitas <beier@pik-potsdam.de>
2021-06-23 10:47:10 +0200

    bugfix in convertIMPACTIrrigInvCost: USA not available -> DEU taken for missing conversion factors

--- R/convertIMPACTIrrigInvCosts.R
+++ R/convertIMPACTIrrigInvCosts.R
@@ -23,13 +23,13 @@
 convertIMPACTIrrigInvCosts <- function(x) {
 
   # convert from bio. to mio. USD
-  x   <- x * 1000
+  x <- x * 1000
 
   # Disaggregate to iso-countries
   # weight for disaggregation: GDP
   w <- collapseNames(setYears(calcOutput("GDPppp", GDPpppFuture = "SRES_SSP_completed",
-    naming = "indicator.scenario", aggregate = FALSE)[, "y2020", "SSP2"],
-  NULL))
+                                          naming = "indicator.scenario", aggregate = FALSE)[, "y2020", "SSP2"],
+                              NULL))
 
   # IMPACT country-region-mapping
   mapping <- read.csv("regionmapping_IMPACT.csv")
@@ -39,23 +39,26 @@ convertIMPACTIrrigInvCosts <- function(x) {
   mapping <- mapping[which(mapping$iso != setdiff(mapping$iso, getRegions(w))), ]
 
   tmp <- new.magpie(cells_and_regions = setdiff(mapping$region, getRegions(x)),
-    years = getYears(x),
-    names = getNames(x),
-    fill = 0)
-  x   <- mbind(x, tmp)
-  x   <- x[intersect(mapping$region, getCells(x)), , ]
+                    years = getYears(x),
+                    names = getNames(x),
+                    fill = 0)
+  x <- mbind(x, tmp)
+  x <- x[intersect(mapping$region, getCells(x)), , ]
 
   x <- toolAggregate(x, rel = mapping, weight = w, from = "region", to = "iso", dim = 1)
 
   # convert 2000 to 2005 USD
   tmp <- convertGDP(x, unit_in = "constant 2000 US$MER", unit_out = "constant 2005 US$MER")
 
-  ### for missing countries use USA rate for now ###
-  tmp[where(is.na(tmp))$true$regions, , ] <- x[where(is.na(tmp))$true$regions, , ] * 
-                                             setYears(tmp["USA", 2016, ] / x["USA", 2016, ], 
+  ### for missing countries use DEU rate for now ###
+  tmp[where(is.na(tmp))$true$regions, , ] <- x[where(is.na(tmp))$true$regions, , ] *
+                                             setYears(tmp["DEU", "y2020", ] / x["DEU", "y2020", ],
                                                       NULL)
   x <- tmp
-  ### for missing countries use USA rate for now ###
+  ### for missing countries use DEU rate for now ###
+
+  # fill missing countries with 0
+  x <- toolCountryFill(x, fill = 0)
 
   return(x)
 }
