5ea2fde2480a4bc7a935092b13e5c916f3a5745d
emolinab <mbacca@pik-potsdam.de>
2022-03-16 09:40:42 +0100

    name fix to camelcase VoP functions

--- R/calcValidCostsOverall.R
+++ R/calcValidCostsOverall.R
@@ -1,5 +1,6 @@
 #' @title calcValidCostsOverall
-#' @description Returns historical values of the overall value of production for the Agricultural, Forestry sectors, and fisheries (mio.05USD)
+#' @description Returns historical values of the overall value of production for the Agricultural, 
+#' Forestry sectors, and fisheries (mio.05USD)
 #' @param datasource datasource for validation. FAO Value of Production dataset.
 #' @return List of magpie object with results on country level, no weight, unit and description.
 #' @author Edna J. Molina Bacca
@@ -7,38 +8,39 @@
 #' \dontrun{
 #' calcOutput("calcValidCostsOverall")
 #' }
+#'
+calcValidCostsOverall <- function(datasource = "FAO") {
 
-calcValidCostsOverall <- function(datasource="FAO") {
+  #### Value of Production for the agriculture, forestry and fisheries sector
 
-  ####Value of Production for the agriculture, forestry and fisheries sector
+  if (datasource == "FAO") {
+  
+  # Original costs in 2020 values. Conversion factor for US$D05
+  x <- dimSums(calcOutput("VoPAFF", aggregate = FALSE), dim = 3.1) / (1 + 0.04)^15
 
-  if(datasource == "FAO"){
+  # Fraction of revenue
 
-  x<-dimSums(calcOutput("VoP_AFF",aggregate = FALSE),dim=3.1)/(1+0.04)^15 # Original costs in 2020 values. Conversion factor for US$D05.
+  rev <- readSource("TFPUSDA")[, , "revenue"]
+  rev <- time_interpolate(rev, interpolated_year = c((getYears(rev, as.integer = TRUE) + 5)),
+                    extrapolation_type = "constant", integrate_interpolated_years = TRUE)
 
-  #Fraction of reven
+  years <- intersect(getYears(x), getYears(rev))
 
-  rev<-readSource("TFP_USDA")[,,"revenue"]
-  rev<-time_interpolate(rev,interpolated_year = c((getYears(rev,as.integer = TRUE)+5)),
-                    extrapolation_type = "constant",integrate_interpolated_years = TRUE)
-
-  years<-intersect(getYears(x),getYears(rev))
-
-  x<-x[,years,]*(1-rev[,years,])
+  x <- x[, years, ] * (1 - rev[, years, ])
 
   getNames(x) <- paste0("Costs|Gross value of production (million US$05/yr)")
-  x<- add_dimension(x, dim=3.1, add="scenario", nm="historical")
-  x<- add_dimension(x, dim=3.2, add="model", nm=datasource)
+  x <- add_dimension(x, dim = 3.1, add = "scenario", nm = "historical")
+  x <- add_dimension(x, dim = 3.2, add = "model", nm = datasource)
 
-  weight=NULL
-  units="(million US$05/yr)"
+  weight <- NULL
+  units <- "(million US$05/yr)"
 
-  }else{
+  } else {
     warning("Only FAO datasource available")
   }
 
-  return(list(x=x,weight=weight,
-              unit=units,
-              description="Validation for overall costs indicators(million US$05/yr)"))
+  return(list(x = x, weight = weight,
+              unit = units,
+              description = "Validation for overall costs indicators(million US$05/yr)"))
 
 }
