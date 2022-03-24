cda05284e77d49acb0ff94473a9ae294df5be700
Marcos Alves <mppalves@gmail.com>
2022-03-24 18:19:26 +0100

    Adjusting past tau hisotorical values for IND, PAK BGD

--- R/calcValidTauPastr.R
+++ R/calcValidTauPastr.R
@@ -16,11 +16,6 @@ calcValidTauPastr <-  function(){
   prod <- calcOutput("GrasslandBiomass", aggregate = F)[, past, "pastr"]
   prod <- toolCountryFill(prod, fill = 0)
   
-  # areas
-  pastr_weight <- calcOutput("PastureSuit",
-                             subtype = paste("ISIMIP3bv2", "MRI-ESM2-0", "1850_2100", sep = ":"),
-                             aggregate = F
-  )[, past, 1]
   # regional mapping
   cell2reg <- toolGetMapping("clustermapping.csv", type = "regional")
   
@@ -28,18 +23,23 @@ calcValidTauPastr <-  function(){
   area <- calcOutput("LUH2v2", landuse_types = "LUH2v2", cellular = F, aggregate = F)[, past, "pastr"]
   area <- toolCountryFill(area, fill = 0)
   
-  # Adding 'otherland' as an extra source of grass biomass comparable to managed pastures in India.
+  # Adding 'otherland' as an extra source of grass biomass comparable to managed pastures in India, Pakistan and Bangladesh.
   otherland <- calcOutput("LUH2v2", landuse_types = "LUH2v2", cellular = F, aggregate = F)[, past, c("secdn", "primn")]
-  area["IND",,"pastr"] <- area["IND",,"pastr"] + setNames(dimSums(otherland["IND",,], dim = 3), 'pastr')
+  area["IND", , "pastr"] <- area["IND", , "pastr"] + setNames(dimSums(otherland["IND", , ], dim = 3), "pastr")
+  area["BGD", , "pastr"] <- area["BGD", , "pastr"] + setNames(dimSums(otherland["BGD", , ], dim = 3), "pastr")
+  area["PAK", , "pastr"] <- area["PAK", , "pastr"] + setNames(dimSums(otherland["PAK", , ], dim = 3), "pastr")
   
   # Actual yields
   yact <- prod[, past, ] / area[, past, ]
   yact[is.nan(yact) | is.infinite(yact)] <- 0
   
   # reference yields
-  yref <- calcOutput("GrasslandsYields", lpjml = "lpjml5p2_pasture", climatetype = "MRI-ESM2-0:ssp370", subtype = "/co2/Nreturn0p5/limN" ,
+  yref <- calcOutput("GrasslandsYields",
+                     lpjml = "lpjml5p2_pasture", climatetype = "MRI-ESM2-0:ssp370", subtype = "/co2/Nreturn0p5/limN",
                      lsu_levels = c(seq(0, 2.2, 0.2), 2.5), past_mngmt = "me2",
-                     aggregate = F)[, past, "pastr.rainfed"]
+                     aggregate = F
+  )[, past, "pastr.rainfed"]
+  
   yref_weights <- calcOutput("LUH2v2", landuse_types = "LUH2v2", cellular = T, aggregate = F)[, past, "pastr"]
   yref <- toolAggregate(yref, rel = cell2reg, from = "cell", to = "country", weight = yref_weights)
   yref <- toolCountryFill(yref, fill = 0)
