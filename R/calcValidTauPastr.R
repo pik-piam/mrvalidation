853276a71ddb11a610c917549a06e0fb18953fc0
mppalves <mppalves@gmail.com>
2022-03-29 15:17:38 +0200

    lint calcvaldtaupastr

--- R/calcValidTauPastr.R
+++ R/calcValidTauPastr.R
@@ -8,68 +8,95 @@
 #' calcOutput("ValidTauPastr")
 #' }
 #'
-
-calcValidTauPastr <-  function(){
-  
+calcValidTauPastr <- function() {
   past <- findset("past")
   # Production
   prod <- calcOutput("GrasslandBiomass", aggregate = F)[, past, "pastr"]
   prod <- toolCountryFill(prod, fill = 0)
-  
+
   # regional mapping
   cell2reg <- toolGetMapping("clustermapping.csv", type = "regional")
-  
+
   # pasture areas
-  area <- calcOutput("LUH2v2", landuse_types = "LUH2v2", cellular = F, aggregate = F)[, past, "pastr"]
+  area <- calcOutput("LUH2v2",
+    landuse_types = "LUH2v2",
+    cellular = F, aggregate = F
+  )[, past, "pastr"]
   area <- toolCountryFill(area, fill = 0)
-  
-  # Adding 'otherland' as an extra source of grass biomass comparable to managed pastures in India, Pakistan and Bangladesh.
-  otherland <- calcOutput("LUH2v2", landuse_types = "LUH2v2", cellular = F, aggregate = F)[, past, c("secdn", "primn")]
-  area["IND", , "pastr"] <- area["IND", , "pastr"] + setNames(dimSums(otherland["IND", , ], dim = 3), "pastr")
-  area["BGD", , "pastr"] <- area["BGD", , "pastr"] + setNames(dimSums(otherland["BGD", , ], dim = 3), "pastr")
-  area["PAK", , "pastr"] <- area["PAK", , "pastr"] + setNames(dimSums(otherland["PAK", , ], dim = 3), "pastr")
-  
+
+  # Adding 'otherland' as an extra source of grass biomass comparable
+  # to managed pastures in India, Pakistan and Bangladesh.
+  otherland <-
+    calcOutput("LUH2v2",
+      landuse_types = "LUH2v2",
+      cellular = F, aggregate = F
+    )[, past, c("secdn", "primn")]
+  area["IND", , "pastr"] <-
+    area["IND", , "pastr"] +
+    setNames(dimSums(otherland["IND", , ], dim = 3), "pastr")
+  area["BGD", , "pastr"] <-
+    area["BGD", , "pastr"] +
+    setNames(dimSums(otherland["BGD", , ], dim = 3), "pastr")
+  area["PAK", , "pastr"] <-
+    area["PAK", , "pastr"] +
+    setNames(dimSums(otherland["PAK", , ], dim = 3), "pastr")
+
   # Actual yields
   yact <- prod[, past, ] / area[, past, ]
   yact[is.nan(yact) | is.infinite(yact)] <- 0
-  
+
   # reference yields
   yref <- calcOutput("GrasslandsYields",
-                     lpjml = "lpjml5p2_pasture", climatetype = "MRI-ESM2-0:ssp370", subtype = "/co2/Nreturn0p5/limN",
-                     lsu_levels = c(seq(0, 2.2, 0.2), 2.5), past_mngmt = "me2",
-                     aggregate = F
+    lpjml = "lpjml5p2_pasture",
+    climatetype = "MRI-ESM2-0:ssp370",
+    subtype = "/co2/Nreturn0p5/limN",
+    lsu_levels = c(seq(0, 2.2, 0.2), 2.5), past_mngmt = "me2",
+    aggregate = F
   )[, past, "pastr.rainfed"]
-  
-  yref_weights <- calcOutput("LUH2v2", landuse_types = "LUH2v2", cellular = T, aggregate = F)[, past, "pastr"]
-  yref <- toolAggregate(yref, rel = cell2reg, from = "cell", to = "country", weight = yref_weights)
+
+  yref_weights <- calcOutput("LUH2v2",
+    landuse_types = "LUH2v2", cellular = T,
+    aggregate = F
+  )[, past, "pastr"]
+  yref <- toolAggregate(yref,
+    rel = cell2reg, from = "cell",
+    to = "country", weight = yref_weights
+  )
   yref <- toolCountryFill(yref, fill = 0)
-  
+
   # tau calculation
   t <- yact[, past, ] / yref[, past, ]
   t[is.nan(t) | is.infinite(t)] <- 0
   t <- collapseNames(t)
-  
+
   # replacing unrealistic high tau values by regional averages
   reg_map <- toolGetMapping("regionmappingH12.csv", type = "cell")
-  t_reg <- toolAggregate(t, rel = reg_map, weight = area, from = "CountryCode", to = "RegionCode")
+  t_reg <- toolAggregate(t,
+    rel = reg_map, weight = area,
+    from = "CountryCode", to = "RegionCode"
+  )
   regions <- reg_map$RegionCode
   names(regions) <- reg_map[, "CountryCode"]
-  
+
   largeTC <- where(t >= 10)$true$individual # tau threshold
   colnames(largeTC)[1] <- "country"
   largeTC <- as.data.frame(largeTC)
-  
+
   for (i in as.vector(largeTC[, "country"])) {
     for (j in as.vector(largeTC[largeTC$country == i, "year"])) {
       t[i, j, ] <- t_reg[regions[i], j, ]
     }
   }
-  
+
   names(dimnames(t))[3] <- "scenario.model.variable"
-  getNames(t) <- "historical.MAgPIEOwn.Productivity|Landuse Intensity Indicator Tau managed pastures (Index)"
-  
-  return(list(x = t,
-              weight = yref * area, # Xref
-              unit="-",
-              description="Historic Trends in managed pastures Land Use Intensity Tau based on FAO yield trends") )
+  getNames(t) <-
+    "historical.MAgPIEOwn.Productivity|Landuse Intensity Indicator Tau managed pastures (Index)" # nolint
+
+  return(list(
+    x = t,
+    weight = yref * area, # Xref
+    unit = "-",
+    description =
+      "Historic Trends in managed pastures Land Use Intensity Tau based on FAO yield trends" # nolint
+  ))
 }
