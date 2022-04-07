dbc2b3e65e8aecda11b0b85c48c763fc284ba908
emolinab <mbacca@pik-potsdam.de>
2022-04-07 17:00:53 +0200

    fix warning calcValidCostsOverall about revenue

--- R/calcValidCostsOverall.R
+++ R/calcValidCostsOverall.R
@@ -1,5 +1,5 @@
 #' @title calcValidCostsOverall
-#' @description Returns historical values of the overall value of production for the Agricultural, 
+#' @description Returns historical values of the overall value of production for the Agricultural,
 #' Forestry sectors, and fisheries (mio.05USD)
 #' @param datasource datasource for validation. FAO Value of Production dataset.
 #' @return List of magpie object with results on country level, no weight, unit and description.
@@ -14,19 +14,10 @@ calcValidCostsOverall <- function(datasource = "FAO") {
   #### Value of Production for the agriculture, forestry and fisheries sector
 
   if (datasource == "FAO") {
-  
-  # Original costs in 2020 values. Conversion factor for US$D05
-  x <- dimSums(calcOutput("VoPAFF", aggregate = FALSE), dim = 3.1) / (1 + 0.04)^15
 
-  # Fraction of revenue
+  # Value of Production in mio. 05USDmer units
+  x <- dimSums(calcOutput("VoPAFF", aggregate = FALSE), dim = 3.1)
 
-  rev <- readSource("TFPUSDA")[, , "revenue"]
-  rev <- time_interpolate(rev, interpolated_year = c((getYears(rev, as.integer = TRUE) + 5)),
-                    extrapolation_type = "constant", integrate_interpolated_years = TRUE)
-
-  years <- intersect(getYears(x), getYears(rev))
-
-  x <- x[, years, ] * (1 - rev[, years, ])
 
   getNames(x) <- paste0("Costs|Gross value of production (million US$05/yr)")
   x <- add_dimension(x, dim = 3.1, add = "scenario", nm = "historical")
