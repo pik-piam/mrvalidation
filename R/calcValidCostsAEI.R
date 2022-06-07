8701bf12704232be0369277843f82193000ea155
Felicitas <beier@pik-potsdam.de>
2022-06-07 15:58:37 +0200

    turn around data dimensions in third dimension for consistency

--- R/calcValidCostsAEI.R
+++ R/calcValidCostsAEI.R
@@ -1,5 +1,5 @@
 #' @title       calcValidCostsAEI
-#' @description calculates the validation data for irrigation investment costs
+#' @description provides the validation data for irrigation investment costs
 #'
 #' @param datasource Datasource of validation data
 #'
@@ -16,8 +16,8 @@ calcValidCostsAEI <- function(datasource = "IMPACT") {
   if (datasource == "IMPACT") {
 
     out <- readSource("IMPACTIrrigInvCosts", convert = TRUE)
-    out <- add_dimension(out, dim = 3.2, add = "variable", nm = "Costs|AEI")
-    out <- add_dimension(out, dim = 3.1, add = "model", nm = datasource)
+    out <- add_dimension(out, dim = 3.2, add = "model", nm = datasource)
+    out <- add_dimension(out, dim = 3.3, add = "variable", nm = "Costs|AEI")
 
     getNames(out) <- paste(getNames(out), "(million US$05/yr)", sep = " ")
     unit          <- "million US$05/yr"
