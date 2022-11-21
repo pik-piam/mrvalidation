20c66ef41a47f7f28b804c3bc5dcbbcdfe41b4c6
caviddhen <david.chen@pik.potsdam.de>
2022-11-21 22:07:17 +0100

    update validTrade bugfix

--- R/calcValidTrade.R
+++ R/calcValidTrade.R
@@ -1,11 +1,12 @@
 #' @title calcValidTrade
 #' @description calculates the validation data for trade of agricultural products
-#' 
-#' @param datasource Datasource of validation data. 
+#'
+#' @param datasource Datasource of validation data.
 #' @param detail if FALSE, only larger product categories are reported
 #' @param nutrient The nutrient in which the results shall be reported.
-#' @param net_trade Net trade flows or total trade 
-#' @param equalized numbers changed so that global production meets global demand (in reality different because of time-delay between exports and imports)
+#' @param net_trade Net trade flows or total trade
+#' @param equalized numbers changed so that global production meets global demand
+#'  (in reality different because of time-delay between exports and imports)
 #'
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
 #' @author Benjamin Leon Bodirsky, Xiaoxi Wang
@@ -13,85 +14,98 @@
 #' \code{\link{calcFAOmassbalance}},
 #' \code{\link{calcValidDemand}}
 #' @examples
-#' 
-#' \dontrun{ 
+#' \dontrun{
 #' calcOutput("ValidTrade")
 #' }
-#' 
-#' @importFrom magpiesets reporthelper summationhelper
+#'
+#' @importFrom magpiesets reporthelper summationhelper findset
 
-calcValidTrade<-function(datasource="FAO", detail=T,nutrient="dm", net_trade=TRUE, equalized = TRUE){
-  
-  if(net_trade){
-    if(datasource=="FAO"){
-      k_trade<-findset("k_trade")
-      mb<-collapseNames(calcOutput("FAOmassbalance",aggregate = F)[,,nutrient])
-      
+calcValidTrade <- function(datasource = "FAO", detail = TRUE, nutrient = "dm", net_trade = TRUE, equalized = TRUE) { # nolint
+
+  if (net_trade) {
+    if (datasource == "FAO") {
+      kTrade <- findset("k_trade")
+      mb <- collapseNames(calcOutput("FAOmassbalance", aggregate = FALSE)[, , nutrient][, , kTrade])
+
       # exports
-      mb<-collapseNames(mb[,,c("production")])-collapseNames(mb[,,"domestic_supply"])
-      
+      mb <- collapseNames(mb[, , c("production")]) - collapseNames(mb[, , "domestic_supply"])
+
       ### normalize trade for global production/demand mismatch
-      .f <- function(x,weight=NULL){
-        y <- dimSums(x,dim=1) # net global trade, so demand/supply mismatch
-        if(is.null(weight)){ # absolute balanceflow for each country the same
-          z <- y/length(getRegions(x))
-        }else if(weight=="net_trade"){ # balanceflow proportional to countries im- or exports
-          z <- abs(x)/dimSums(abs(x),dim=1) * y
+      .f <- function(x, weight = NULL) {
+        y <- dimSums(x, dim = 1) # net global trade, so demand/supply mismatch
+        if (is.null(weight)) { # absolute balanceflow for each country the same
+          z <- y / length(getItems(x, dim = 1))
+        } else if (weight == "net_trade") { # balanceflow proportional to countries im- or exports
+          z <- abs(x) / dimSums(abs(x), dim = 1) * y
           z[is.na(z)] <- 0
-        }
-        else{stop("no weight exits")}
-        return(x-z)
+        } else {
+stop("no weight exits")
+}
+        return(x - z)
       }
-      
-      if(equalized){
-        normalized_trade = .f(mb)
+
+      if (equalized) {
+        normalizedTrade <- .f(mb)
       } else {
-        normalized_trade = mb
+        normalizedTrade <- mb
       }
-      
-      out<-reporthelper(x=normalized_trade,dim = 3.1,level_zero_name = "Net-Trade",detail = detail)
-      
-    } else stop("No data exist for the given datasource!")
+
+      out <- reporthelper(x = normalizedTrade, dim = 3.1, level_zero_name = "Net-Trade", detail = detail)
+
+    } else {
+stop("No data exist for the given datasource!")
+}
   } else {
-    
-    k_trade<-findset("k_trade")
-    mb <- calcOutput("FAOmassbalance",aggregate = F)
-    mb <- collapseNames(mb[,,nutrient])
-    
-    exports <- collapseNames(mb[,,c("export")])
-    imports <- collapseNames(mb[,,c("import")])
-    net_trade <- collapseNames(mb[,,c("production")])-collapseNames(mb[,,"domestic_supply"]-collapseNames(mb[,,"stock_variation"]))
-    
-    check <- exports-imports-net_trade
+
+    kTrade <- findset("k_trade")
+    mb <- calcOutput("FAOmassbalance", aggregate = FALSE)
+    mb <- collapseNames(mb[, , nutrient][, , kTrade])
+
+    exports <- collapseNames(mb[, , c("export")])
+    imports <- collapseNames(mb[, , c("import")])
+    netTrade <- collapseNames(mb[, , c("production")]) - collapseNames(mb[, , "domestic_supply"] -
+                                                          collapseNames(mb[, , "stock_variation"]))
+
+    check <- exports - imports - netTrade
     message("inconsistencies in massbalances exist before year 2000")
-    check_years=c("y2000", "y2005", "y2010")
-    mismatch = unique(magclass::where(abs(check[,check_years,]) > 0.1)$true$individual[,3])
-    if (length(mismatch>0)){
-      message(paste(c("larger mismatch between absolute trade and net-trade for the products:",mismatch),collapse = " "))
+    checkYears <- c("y2000", "y2005", "y2010")
+    mismatch <- unique(magclass::where(abs(check[, checkYears, ]) > 0.1)$true$individual[, 3])
+    if (length(mismatch > 0)) {
+      message(paste(c("larger mismatch between absolute trade and net-trade for the products:", mismatch),
+                      collapse = " "))
     }
 
-    if(equalized){ stop("no equalized absolute trade implemented yet. You should set equalized to FALSE")}
-    
-    out1<-reporthelper(exports,dim = 3.1,level_zero_name = "Exports",detail = detail)
-    out2<-reporthelper(imports,dim = 3.1,level_zero_name = "Imports",detail = detail)
-    out<-mbind(out1,out2)
-  }
-  
-  out <- summationhelper(out,excludeLevels = 1)
-  out <- add_dimension(out, dim=3.1, add="scenario", nm="historical")
-  out <- add_dimension(out, dim=3.2, add="model", nm=datasource)
-  names(dimnames(out))[3] <- "scenario.model.variable"
-  if(nutrient=="dm"){unit="Mt DM/yr"
-  } else if (nutrient=="nr"){unit="Mt Nr/yr"
-  } else if (nutrient=="p"){unit="Mt P/yr"
-  } else if (nutrient=="k"){unit="Mt K/yr"
-  } else if (nutrient=="ge"){unit="PJ/yr"
-  } else if (nutrient=="wm"){unit="Mt WM/yr"}
-  getNames(out) <- paste0(sub("\\|$","",getNames(out))," (",unit,")") 
+    if (equalized) {
+ stop("no equalized absolute trade implemented yet. You should set equalized to FALSE")
+}
 
-  return(list(x=out,
-              weight=NULL,
-              unit=unit,
-              description="Agricultural Trade")
+    out1 <- reporthelper(exports, dim = 3.1, level_zero_name = "Exports", detail = detail)
+    out2 <- reporthelper(imports, dim = 3.1, level_zero_name = "Imports", detail = detail)
+    out <- mbind(out1, out2)
+  }
+
+  out <- summationhelper(out, excludeLevels = 1)
+  out <- add_dimension(out, dim = 3.1, add = "scenario", nm = "historical")
+  out <- add_dimension(out, dim = 3.2, add = "model", nm = datasource)
+  names(dimnames(out))[3] <- "scenario.model.variable"
+  if (nutrient == "dm") {
+unit <- "Mt DM/yr"
+  } else if (nutrient == "nr") {
+unit <- "Mt Nr/yr"
+  } else if (nutrient == "p") {
+unit <- "Mt P/yr"
+  } else if (nutrient == "k") {
+unit <- "Mt K/yr"
+  } else if (nutrient == "ge") {
+unit <- "PJ/yr"
+  } else if (nutrient == "wm") {
+unit <- "Mt WM/yr"
+}
+  getNames(out) <- paste0(sub("\\|$", "", getNames(out)), " (", unit, ")")
+
+  return(list(x = out,
+              weight = NULL,
+              unit = unit,
+              description = "Agricultural Trade")
          )
 }
