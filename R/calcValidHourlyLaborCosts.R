da44fcd6c870229d5601479dad816ebdef2c9a45
Debbora Leip <leip@pik-potsdam.de>
2023-10-09 22:06:49 +0200

    updated wage validation

--- R/calcValidHourlyLaborCosts.R
+++ R/calcValidHourlyLaborCosts.R
@@ -2,36 +2,69 @@
 #'
 #' @description hourly labor costs in crop+livestock production
 #'
-#' @param datasource So far only "ILO_completed" (based on regression between ILO hourly labor costs and GDP pc MER,
-#' calibrated to match USDA/FAO total labor costs)
-#'
+#' @param datasource Available datasources are:
+#' \itemize{
+#' \item ILO_raw : ILO hourly labor costs data
+#' \item ILO_completed : ILO hourly labor costs data completed with a regression with GDP pc MER
+#' \item USDA_FA0_raw : USDA/FAO hourly labor costs data
+#' \item USDA_FA0_completed : USDA/FAO hourly labor costs data completed with a regression with GDP pc MER
+#' }
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
 #' @author Debbora Leip
 #'
 #' @examples
 #' \dontrun{
-#' calcOutput("ValidHourlyLaborCosts", datasource="ILO_completed")
+#' calcOutput("ValidHourlyLaborCosts", datasource = "ILO_completed")
 #' }
 #'
 
 calcValidHourlyLaborCosts <- function(datasource = "ILO_completed") {
 
   if (datasource == "ILO_completed") {
-    hourlyCosts <- calcOutput("HourlyLaborCosts", datasource = "USDA_FAO", aggregate = FALSE)
-    hourlyCosts <- setNames(hourlyCosts, "Labor|Wages|Hourly labor costs (USDMER05/h)")
-
-    # total hours worked in 2010 as weight for aggregation to world regions
-    agEmpl <- calcOutput("AgEmplILO", aggregate = FALSE, subsectors = FALSE)
-    weeklyHours <- calcOutput("WeeklyHoursILO", aggregate = FALSE)
-    weight <- hourlyCosts
-    weight[, , ] <- agEmpl[, 2010, ] * weeklyHours[, 2010, ]
+    hourlyCosts <- calcOutput("HourlyLaborCosts", datasource = "ILO", fillWithRegression = TRUE,
+                              calibYear = NULL, cutAfterCalibYear = FALSE, aggregate = FALSE)
 
     description <- paste("Hourly labor costs in agriculture (based on ILO data completed with a",
-                           "regression with GDP, calibrated using USDA and FAO data)")
+                         "regression with GDP)")
+
+  } else if (datasource == "ILO_raw") {
+    hourlyCosts <- calcOutput("HourlyLaborCosts", datasource = "ILO",
+                              fillWithRegression = FALSE, aggregate = FALSE)
+
+    description <- paste("Hourly labor costs in agriculture (based on ILO data completed with a",
+                         "regression with GDP)")
+
+  } else if (datasource == "USDA_FAO_completed") {
+    hourlyCosts <- calcOutput("HourlyLaborCosts", datasource = "USDA_FAO", fillWithRegression = TRUE,
+                              calibYear = NULL, cutAfterCalibYear = FALSE, aggregate = FALSE)
+
+    description <- paste("Hourly labor costs in agriculture (based on ILO data completed with a",
+                         "regression with GDP)")
+
+  } else if (datasource == "USDA_FAO_raw") {
+    hourlyCosts <- calcOutput("HourlyLaborCosts", datasource = "USDA_FAO",
+                              fillWithRegression = FALSE, aggregate = FALSE)
+
+    description <- paste("Hourly labor costs in agriculture (based on USDA/FAO data completed with a",
+                         "regression with GDP)")
   } else {
     stop("Datsource not available")
   }
 
+  hourlyCosts <- setNames(hourlyCosts, "Labor|Wages|Hourly labor costs (USDMER05/h)")
+
+  # total hours worked as weight for aggregation to world regions
+  agEmpl <- calcOutput("AgEmplILO", aggregate = FALSE, subsectors = FALSE)
+  weeklyHours <- calcOutput("WeeklyHoursILO", aggregate = FALSE)
+  weight <- hourlyCosts
+  years <- intersect(getYears(hourlyCosts, as.integer = TRUE),
+                     intersect(getYears(agEmpl, as.integer = TRUE), getYears(weeklyHours, as.integer = TRUE)))
+  weight[, years, ] <- agEmpl[, years, ] * weeklyHours[, years, ]
+  weight[, setdiff(getYears(hourlyCosts,
+                            as.integer = TRUE), years), ] <- agEmpl[, max(years), ] * weeklyHours[, max(years), ]
+  weight[hourlyCosts == 0] <- 0
+
+  # add dimensions
   out <- add_dimension(hourlyCosts, dim = 3.1, add = "scenario", nm = "historical")
   out <- add_dimension(out, dim = 3.2, add = "model", nm = datasource)
 
--- R/calcValidWageDevelopment.R
+++ R/calcValidWageDevelopment.R
@@ -2,8 +2,13 @@
 #'
 #' @description wage index: hourly labor costs in crop+livestock production relative to a baseyear
 #'
-#' @param datasource So far only "ILO_completed" (based on regression between ILO hourly labor costs and GDP pc MER,
-#' calibrated to match USDA/FAO total labor costs)
+#' @param datasource Available datasources are:
+#' \itemize{
+#' \item ILO_raw : based on ILO hourly labor costs data
+#' \item ILO_completed : based on ILO hourly labor costs data completed with a regression with GDP pc MER
+#' \item USDA_FA0_raw : based on USDA/FAO hourly labor costs data
+#' \item USDA_FA0_completed : based on USDA/FAO hourly labor costs data completed with a regression with GDP pc MER
+#' }
 #' @param baseYear year relative to which the wage development should be calculated
 #' @return List of magpie objects with results on country level, weight on country level, unit and description.
 #' @author Debbora Leip
@@ -16,24 +21,19 @@
 
 calcValidWageDevelopment <- function(datasource = "ILO_completed", baseYear = 2000) {
 
-  if (datasource == "ILO_completed") {
-    hourlyCosts <- calcOutput("HourlyLaborCosts", datasource = "USDA_FAO", aggregate = FALSE)
+  hourlyCosts <- setNames(calcOutput("ValidHourlyLaborCosts", datasource = datasource, aggregate = FALSE), NULL)
 
-    if (!baseYear %in% getYears(hourlyCosts, as.integer = TRUE)) stop("Baseyear not available.")
+  if (!baseYear %in% getYears(hourlyCosts, as.integer = TRUE)) stop("Baseyear not available.")
 
-    wageIndex <- collapseDim(hourlyCosts / hourlyCosts[, baseYear, ], dim = 2.2)
-    wageIndex <- setNames(wageIndex, paste0("Labor|Wages|Hourly labor costs relative to ", baseYear, " (index)"))
+  wageIndex <- collapseDim(hourlyCosts / hourlyCosts[, baseYear, ], dim = 2.2)
+  wageIndex <- setNames(wageIndex, paste0("Labor|Wages|Hourly labor costs relative to ", baseYear, " (index)"))
 
-    # population in 2000 as weight for aggregation to world regions and globally
-    weight <- wageIndex
-    weight[, , ] <- calcOutput("Population", naming = "scenario",
-                                aggregate = FALSE, years = c(baseYear))[, , "SSP2", drop = TRUE]
+  # population in 2000 as weight for aggregation to world regions and globally
+  weight <- wageIndex
+  weight[, , ] <- calcOutput("Population", naming = "scenario",
+                             aggregate = FALSE, years = c(baseYear))[, , "SSP2", drop = TRUE]
 
-    description <- paste("Wage index calculated as hourly labor costs in agriculture relative to 2000 (based on ILO",
-                           "data completed with a regression with GDP, calibrated using USDA and FAO data)")
-  } else {
-    stop("Datsource not available")
-  }
+  description <- paste("Wage index calculated as hourly labor costs in agriculture relative to 2000")
 
   out <- add_dimension(wageIndex, dim = 3.1, add = "scenario", nm = "historical")
   out <- add_dimension(out, dim = 3.2, add = "model", nm = datasource)
