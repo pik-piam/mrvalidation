62c4c739b7bab358690190533ade643f88a84207
caviddhen <david.chen@pik.potsdam.de>
2023-10-23 11:10:46 +0200

    fix mappings warning validCostsTranspor

--- R/calcValidCostsTransport.R
+++ R/calcValidCostsTransport.R
@@ -114,7 +114,7 @@ calcValidCostsTransport <- function(datasource = "GTAPtransport") {
 
     out <- costs[, , products] * productionDistance[, , products]
 
-    mapping <- toolGetMapping(name = "CountryToCellMapping.csv", type = "cell")
+    mapping <- toolGetMapping(name = "CountryToCellMapping.csv", type = "cell", where = "mappingfolder")
     out   <- toolAggregate(out, rel = mapping, from = "celliso", to = "iso", dim = 1)
     out   <- toolCountryFill(out, fill = 0)
 
