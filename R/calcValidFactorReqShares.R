84ee5230f08d76c768ced2e5316a6cc418582c49
Debbora Leip <leip@pik-potsdam.de>
2024-03-12 15:19:15 +0100

    fix in aggregation weight for factor requirement shares

--- R/calcValidFactorReqShares.R
+++ R/calcValidFactorReqShares.R
@@ -30,14 +30,26 @@ calcValidFactorReqShares <- function(subtype = "crop") {
   out <- add_dimension(out, dim = 3.2, add = "model", nm = "USDA")
 
 
-  # production as weight (0 weight to countries with unexpectedly high capital shares)
-  weight <- dimSums(collapseDim(calcOutput("Production", aggregate = FALSE)[, , "dm"]), dim = 3.1)
+  # factor costs as weight
+  factorCostsCrops <- dimSums(calcOutput("FactorCostsCrops", aggregate = FALSE), dim = 3.1)
+  factorCostsLivst <- dimSums(calcOutput("FactorCostsLivst", aggregate = FALSE), dim = 3.1)
+  weight <- factorCostsCrops + factorCostsLivst
+
+  # add missing years to weight
+  missingYears <- setdiff(getYears(out, as.integer = TRUE), getYears(weight, as.integer = TRUE))
+  if (any(missingYears > min(getYears(weight, as.integer = TRUE)))) {
+    stop("Need to fix weight for new years not covered in factor costs dataset")
+  }
+  weight <- magpiesort(add_columns(weight, dim = 2, addnm = paste0("y", missingYears)))
+  weight[, missingYears, ] <- weight[, min(getYears(weight, as.integer = TRUE)), ]
+
+  # Give 0 weigh to countries with unexpectedly high capital shares
   weight[c("BLZ", "CRI", "DOM", "HND", "JAM", "MEX", "NIC", "PAN", "SLV"), , ] <- 0
+
   years <- intersect(getYears(weight), getYears(out))
   weight <- weight[, years, ]
   out <- out[, years, ]
   weight[out[, , "Capital", pmatch = TRUE] == 0] <- 0
-
 
   return(list(x = out,
               weight = weight,
