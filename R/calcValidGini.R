922f740b019436ffc9721d344ad86bff74024536
caviddhen <david.chen@pik.potsdam.de>
2024-08-05 16:14:52 +0200

    weight warnings

--- R/calcValidGini.R
+++ R/calcValidGini.R
@@ -7,27 +7,27 @@
 
 calcValidGini <- function() {
 
- out <- readSource("WBPoverty", subtype = "Gini", convert = FALSE)
- getSets(out)[3] <- "scenario"
- getItems(out, dim = 3) <- "historical"
+  out <- readSource("WBPoverty", subtype = "Gini", convert = FALSE)
+  getSets(out)[3] <- "scenario"
+  getItems(out, dim = 3) <- "historical"
 
- weight <- calcOutput("Population", aggregate = FALSE)[, , "pop_SSP2"]
- weight <- time_interpolate(weight,  interpolated_year = getYears(out),
-                                    integrate_interpolated_years = TRUE)
+  weight <- calcOutput("Population", aggregate = FALSE)[, , "pop_SSP2"]
+  weight <- time_interpolate(weight, interpolated_year = getYears(out),
+                             integrate_interpolated_years = TRUE)
 
- weight <- weight[getItems(out, dim = 1), getItems(out, dim = 2), ]
- weight[which(out == 9999)] <- 0 # make missing data 0 weight
+  weight <- weight[getItems(out, dim = 1), getItems(out, dim = 2), ]
+  weight[which(out == 9999)] <- 0 # make missing data 0 weight
 
- out <- add_dimension(out, dim = 3.2, add = "model", nm = "World Bank WDI")
- getNames(out, dim = 1) <- "Income|Gini Coefficient (0-1)"
+  out <- add_dimension(out, dim = 3.2, add = "model", nm = "World Bank WDI")
+  getNames(out, dim = 1) <- "Income|Gini Coefficient (0-1)"
 
- getSets(weight)[3] <- "scenario"
- getItems(weight, dim = 3) <- "historical"
- weight <- add_dimension(weight, dim = 3.2, add = "model", nm = "World Bank WDI")
- getNames(weight, dim = 1) <- "Income|Gini Coefficient (0-1)"
+  getSets(weight)[3] <- "scenario"
+  getItems(weight, dim = 3) <- "historical"
+  weight <- add_dimension(weight, dim = 3.2, add = "model", nm = "World Bank WDI")
+  getNames(weight, dim = 1) <- "Income|Gini Coefficient (0-1)"
 
   return(list(x = out,
-              weight = weight,
+              weight = weight + 10^-10,
               unit = "(0-1) Gini Coefficient between 0 and 1",
               description = "Gini Coefficient")
   )
--- R/calcValidPriceIndex.R
+++ R/calcValidPriceIndex.R
@@ -1,8 +1,8 @@
 #' @title calcValidPriceIndex
 #'
-#' @description provides global producer price index from FAO 
-#' 
-#' @param datasource Options of the source of data:  \code{FAO}. 
+#' @description provides global producer price index from FAO
+#'
+#' @param datasource Options of the source of data:  \code{FAO}.
 #' @param value \code{real} and \code{nominal}
 #' @param baseyear Baseyear for normalizing of price index.
 #' @param round Rounding of price index to intiger numbers. Default \code{TRUE}.
@@ -12,80 +12,86 @@
 #' @seealso
 #' \code{\link{readProdPrIndex}}
 #' @examples
-#' 
-#' \dontrun{ a <- calcOutput("ValidPriceIndex", value="real", aggregate=FALSE)
+#' \dontrun{
+#' a <- calcOutput("ValidPriceIndex", value = "real", aggregate = FALSE)
 #' }
-#' 
+#'
 #' @importFrom magpiesets findset reporthelper
 #' @importFrom magclass wrap setNames
 #' @importFrom madrat toolAggregate toolGetMapping
 
-calcValidPriceIndex <- function(datasource="FAO", value="real", baseyear="y2005", round=TRUE){
-  if(datasource=="FAO"){
+calcValidPriceIndex <- function(datasource = "FAO", value = "real", baseyear = "y2005", round = TRUE) {
+  if (datasource == "FAO") {
     # FAO prices (already in dm values)
-    p_t <- calcOutput("PriceAgriculture", datasource=datasource, aggregate=FALSE)
+    pT <- calcOutput("PriceAgriculture", datasource = datasource, aggregate = FALSE)
     ## set prices p_t back to magpie names
-    p_t <- collapseNames(p_t)
-    
+    pT <- collapseNames(pT)
+
     # FAO production
-    q_t <- calcOutput("FAOharmonized", aggregate=FALSE)
-    aggregation <- toolGetMapping("FAOitems.rda","sectoral",where="mrvalidation")
-    q_t <- toolAggregate(q_t[,,"production"], rel=aggregation, from="FoodBalanceItem", 
-                    to="k", dim=3.1, partrel = TRUE, verbosity=2)
-    q_t <- collapseNames(q_t)
+    qT <- calcOutput("FAOharmonized", aggregate = FALSE)
+    aggregation <- toolGetMapping("FAOitems.rda", "sectoral", where = "mrvalidation")
+    qT <- toolAggregate(qT[, , "production"], rel = aggregation, from = "FoodBalanceItem",
+                        to = "k", dim = 3.1, partrel = TRUE, verbosity = 2)
+    qT <- collapseNames(qT)
     ## convert FAO production to DM tonnes
-    dm <- 1/readSource("ProductAttributes","Products")[,,"wm"]
+    dm <- 1 / readSource("ProductAttributes", "Products")[, , "wm"]
     dm <- collapseNames(dm)
     ### common years and names dm factors, quantities and prices
-    comms <- intersect(intersect(getNames(dm),getNames(q_t)), getNames(p_t))
-    ys <- intersect(getYears(p_t),getYears(q_t))[-1]
-    q_t <- q_t[,ys,comms]*dm[,,comms]
-    p_t <- p_t[,ys,comms]
-    
-    # create baseyear values 
-    q_0 <- q_t
-    p_0 <- p_t
-    for(y in intersect(getYears(p_t),getYears(q_t))){
-      q_0[,y,] <- setYears(q_t[,baseyear,],y)
-      p_0[,y,] <- setYears(p_t[,baseyear,],y)
+    comms <- intersect(intersect(getNames(dm), getNames(qT)), getNames(pT))
+    ys <- intersect(getYears(pT), getYears(qT))[-1]
+    qT <- qT[, ys, comms] * dm[, , comms]
+    pT <- pT[, ys, comms]
+
+    # create baseyear values
+    q0 <- qT
+    p0 <- pT
+    for (y in intersect(getYears(pT), getYears(qT))) {
+      q0[, y, ] <- setYears(qT[, baseyear, ], y)
+      p0[, y, ] <- setYears(pT[, baseyear, ], y)
     }
-    
+
     # calculate Laspeyers price index
-    out <- dimSums(p_t * q_0, dim=3, na.rm=TRUE) / dimSums(p_0 * q_0, dim=3, na.rm=TRUE)
-    
+    out <- dimSums(pT * q0, dim = 3, na.rm = TRUE) / dimSums(p0 * q0, dim = 3, na.rm = TRUE)
+
     out <- out * 100
-    out[is.nan(out)|is.infinite(out)] <- 0
-    if (round) {out <- round(out)}
-    
-    out <- setNames(out,paste0("Prices|Food Price Index (Index ",sub("y","",baseyear),"=100)"))
+    out[is.nan(out) | is.infinite(out)] <- 0
+    if (round) {
+      out <- round(out)
+    }
+
+    out <- setNames(out, paste0("Prices|Food Price Index (Index ", sub("y", "", baseyear), "=100)"))
     names(dimnames(out))[3] <- "variable"
-    
-    #weithts: values of consumption at the initial time step
-    weights <- dimSums(p_0 * q_0, dim=3, na.rm=TRUE)
+
+    # weithts: values of consumption at the initial time step
+    weights <- dimSums(p0 * q0, dim = 3, na.rm = TRUE)
     names(dimnames(weights))[3] <- "variable"
     dimnames(weights)[[3]] <- "Aggregate Initial Consumption Value"
-    
+
     weight       <- weights
-    description  <- paste0("Fao Food Price Index, ",sub("y","",baseyear),"=100")
-    isocountries <- TRUE 
-    unit <- paste0("Index ",baseyear,"=100")
-    
-  } else if(datasource=="ProdPrIndex"){
-    out <- readSource(type="ProdPrIndex")
-    out <- out[,,value]
-    
+    description  <- paste0("Fao Food Price Index, ", sub("y", "", baseyear), "=100")
+    isocountries <- TRUE
+    unit <- paste0("Index ", baseyear, "=100")
+
+  } else if (datasource == "ProdPrIndex") {
+    out <- readSource(type = "ProdPrIndex")
+    out <- out[, , value]
+
     weight       <- NULL
     description  <- "Fao Food Price Index, 2002-2004=100"
     unit         <- "Index 2002-2004=100"
     isocountries <- FALSE
-  } 
-  
-  out <- add_dimension(out, dim=3.1, add="scenario", nm="historical")
-  out <- add_dimension(out, dim=3.2, add="model", nm=datasource)
-  
-  return(list(x=out,
-              weight=weight,
-              unit=unit,
-              description=description,
-              isocountries=isocountries))
+  }
+
+  out <- add_dimension(out, dim = 3.1, add = "scenario", nm = "historical")
+  out <- add_dimension(out, dim = 3.2, add = "model", nm = datasource)
+
+  if (!is.null(weight)) {
+    weight <- weight + 10^-10
+  }
+
+  return(list(x = out,
+              weight = weight,
+              unit = unit,
+              description = description,
+              isocountries = isocountries))
 }
