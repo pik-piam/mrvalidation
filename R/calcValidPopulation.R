66cb32902db065cdcae24fd6f37ea5bb710784c3
Johannes Koch <jokoch@pik-potsdam.de>
2024-10-11 09:10:58 +0200

    Refactor in accordance with mrdrivers update

--- R/calcValidPopulation.R
+++ R/calcValidPopulation.R
@@ -2,53 +2,31 @@
 #'
 #' Returns historical development of population and future projections of population dynamics
 #'
-#'
-#' @param PopulationPast population past data source
-#' @param PopulationFuture population future data source
-#' @param TimeFromFindSet boolean deciding something
+#' @param pastData population past data source
+#' @param futureData population future data source
 #' @return list of magpie object with data and weight
 #' @author Florian Humpenoeder, Jan Philipp Dietrich, David Chen
-#' @importFrom magclass getRegions
-#' @import mrdrivers
-calcValidPopulation <- function(PopulationPast   = "WDI-UN_PopDiv-MI",
-                                PopulationFuture = c("SSPs-UN_PopDiv-MI",
-                                                     "SDPs-UN_PopDiv-MI",
-                                                     "SSP2EU-UN_PopDiv-MI"),
-                                TimeFromFindSet = TRUE) {
+calcValidPopulation <- function(pastData = "WDI-UN_PopDiv-MI", futureData = "SSPs-UN_PopDiv") {
 
-  past   <- calcOutput("PopulationPast",
-                       PopulationPast = PopulationPast,
-                       aggregate = FALSE)
-  getNames(past) <- paste("historical", paste0(PopulationPast), "Population (million people)", sep = ".")
+  past   <- calcOutput("PopulationPast", pastData = pastData, aggregate = FALSE)
+  future <- calcOutput("PopulationFuture", futureData = futureData, aggregate = FALSE)
+
+  getNames(past) <- paste("historical", pastData, "Population (million people)", sep = ".")
+  getNames(future) <- paste(getNames(future), futureData, "Population (million people)", sep = ".")
   getSets(past)[3] <- "scenario.model.variable"
-
-  future <- NULL
-  for (i in 1:length(PopulationFuture)) {
-  tmp <- calcOutput("PopulationFuture",
-                    PopulationFuture = PopulationFuture[i],
-                    extension2150 = "bezier",
-                    aggregate = FALSE)
-  future <- mbind(future, tmp)
-  }
-
-
-  getNames(future) <- paste(sub("^pop_", "", getNames(future)),
-                            paste0(PopulationFuture, collapse = "_and_"),
-                            "Population (million people)",
-                            sep = ".")
   getSets(future)[3] <- "scenario.model.variable"
 
-  out <- new.magpie(getRegions(past),
-                    union(getYears(past), getYears(future)),
-                    c(getNames(past), getNames(future)),
+  out <- new.magpie(cells_and_regions = getItems(past, 1),
+                    years = union(getYears(past), getYears(future)),
+                    names = c(getNames(past), getNames(future)),
+                    sets = c("region", "year", "scenario.model.variable"),
                     fill = NA)
   out[, getYears(past), getNames(past)] <- past
   out[, getYears(future), getNames(future)] <- future
 
-  getSets(out, fulldim = FALSE)[3] <- "scenario.model.variable"
-  return(list(x = out, weight = NULL, unit = "million",
-              description = paste0("Population data. Datasource for the Past: ",
-                                 PopulationPast,
-                                 ". Datasource for the Future: ",
-                                 paste0(PopulationFuture, collapse = ", "))))
+  list(x = out,
+       weight = NULL,
+       unit = "million",
+       description = paste0("Population data. Datasource for the Past: ", pastData, ". Datasource for the Future: ",
+                            futureData))
 }
